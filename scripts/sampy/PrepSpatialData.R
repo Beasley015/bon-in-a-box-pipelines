a961c634eb1faace71a39a475c0b2fed9bbd147b
E. Beasley <beasley.em@gmail.com>
2025-10-07 16:36:30 -0500

    added case file to data prep

--- scripts/sampy/PrepSpatialData.R
+++ scripts/sampy/PrepSpatialData.R
@@ -8,23 +8,25 @@ library(units)
 # Load inputs
 inputs <- fromJSON(file=file.path(outputFolder, "input.json"))
 
+grid <- st_read(inputs$raw_grid)
+
 # K values --------------
 
-# Load hexagonal grid and land cover raster
-grid <- st_read(inputs$raw_grid)
+# Load land cover raster
+K.grid <- grid
 land_cover <- rast(inputs$rasters[1])
 
 land_cover <- round(land_cover, digits=0)
 
 # Change crs of raster, if needed
 print("Reprojecting land cover raster...")
-grid <- st_transform(grid, crs=crs(land_cover))
+K.grid <- st_transform(K.grid, crs=crs(land_cover))
 
 # Get % land cover in each grid cell
 print("Extracting land cover classes...")
 perc_land_cover <- exact_extract(x=land_cover$cec_land_cover_2020,
-                                 y=grid,fun='frac') %>%
-  mutate(id = grid$id)
+                                 y=K.grid,fun='frac') %>%
+  mutate(id = K.grid$id)
 
 # Rename columns
 colnames(perc_land_cover) <- c("conifer", "deciduous",
@@ -40,44 +42,68 @@ k_per_km <- c(2.9, 3.4, 3.2, 3.4, 4, 3.4, 12.5, 3.4, 18, 0)
 wmeans <- apply(perc_land_cover[,-11], 1, 
                 function(x) weighted.mean(k_per_km, w = x))
 
-grid$k <- wmeans
+K.grid$k <- wmeans
 
 # Special combos adjustment
 ag_decid <- which(perc_land_cover$cropland > 0.2 &
                     perc_land_cover$deciduous > 0.2)
-grid$k[ag_decid] <- 18
+K.grid$k[ag_decid] <- 18
 
 decid_urban <- which(perc_land_cover$deciduous > 0.2 &
                        perc_land_cover$urban > 0.2)
 
-grid$k[decid_urban] <- 21
+K.grid$k[decid_urban] <- 21
 
 ag_decid_urban <- which(perc_land_cover$cropland > 0.2 &
                             perc_land_cover$deciduous > 0.2 &
                             perc_land_cover$urban > 0.2)
-grid$k[ag_decid_urban] <- 27
+K.grid$k[ag_decid_urban] <- 27
 
 # Elevation adjustment
 elev <- rast(inputs$rasters[2])
-elev <- project(elev, crs(grid))
+elev <- project(elev, crs(K.grid))
 
 elev.per.hex <- exact_extract(elev, grid, fun='mean') 
 elcell <- which(elev.per.hex >= 500)
 
-grid$k[elcell] <- 1
+K.grid$k[elcell] <- 1
 
 # Adjust for cell area in km
-ar <- st_area(grid[1,])
-ar <- set_units(ar, km^2)
+ar <- st_area(K.grid[1,])
+ar <- set_units(ar, km^2) 
 
-grid$k <- grid$k*ar
-grid$k <- as.numeric(grid$k)
+K.grid$k <- K.grid$k*ar
+K.grid$k <- as.numeric(K.grid$k)
+
+# Cases ----------------
+if(is.null(inputs$cases) == F){
+  cases <- read.csv(file=inputs$cases)
+  
+  case.sf <- st_as_sf(cases, coords = c(2,1))
+  
+  st_crs(case.sf) <- 4326
+  
+  inters <- st_intersects(grid, case.sf)
+  inters.bin <- do.call(c,lapply(inters, function(x) length(x)>0))
+  
+  case.grid <- grid
+  case.grid$case <- 0
+  case.grid$case[inters.bin] <- 1
+}
 
 # Feature to come: Management -------------
 
 # Save as geojson
 K_grid <- file.path(outputFolder, "spatial_features.geojson")
+st_write(K.grid, dsn=K_grid, append = F)
 
-st_write(grid, dsn=K_grid, append = F)
+if(exists("case.grid")==T){
+  case_grid <- file.path(outputFolder, "cases.geojson")
+  st_write(case.grid, dsn=case_grid, append=F)
+}
 
 biab_output("K_grid", K_grid)
+
+if(exists("case_grid")==T){
+  biab_output("case_grid", case_grid)
+}
